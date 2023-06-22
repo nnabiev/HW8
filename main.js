313cdd4559195141aa7382b7f0bd27e9e376ab7c
nnabiev <nnabiev479@gmail.com>
2023-06-22 13:52:03 +0500

    spread

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-const people = [
-    { name: "Rashid", age: 22, budget: 48000 },
-    { name: "Shuhrat", age: 20, budget: 52000 },
-    { name: "Abror", age: 19, budget: 38000 },
-    { name: "Sorbon", age: 23, budget: 25000 },
-    { name: "Shahzod", age: 17, budget: 61000 },
-    { name: "Foruz", age: 18, budget: 31000 },
-  ];
- const abc = people.filter(person=>{
-    if (person.age>=18){
-        return true
-    }
- })
- console.log(abc);
+// const people = [
+//     { name: "Rashid", age: 22, budget: 48000 },
+//     { name: "Shuhrat", age: 20, budget: 52000 },
+//     { name: "Abror", age: 19, budget: 38000 },
+//     { name: "Sorbon", age: 23, budget: 25000 },
+//     { name: "Shahzod", age: 17, budget: 61000 },
+//     { name: "Foruz", age: 18, budget: 31000 },
+//   ];
+sort=[1,2,90,4,5,6]
+sortedValues=sort.toSorted((a,b)=>a-b)
+console.log(sortedValues);
